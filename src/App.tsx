bd85b7baeaac90e89dc4a5dbfaa156c04c0f9fd8
leanarys <mary.leana.n.reyes@gmail.com>
2025-02-10 23:30:25 +0800

    [MLR] - Updated folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Routes, Route, Link } from "react-router-dom";
-import HomeScreen from "./pages/home";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
+function App() {
+  const [count, setCount] = useState(0);
 
-//           I'm working late
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-const Home111: React.FC = () => (
-  <div>
-    <h1>Home Screen1111</h1>
-    <p>Welcome to the home page.</p>
-  </div>
-);
-
-const App: React.FC = () => {
   return (
-    <div>
-      <Routes>
-        <Route path="*" element={<HomeScreen />} />
-        <Route path="/" element={<HomeScreen />} />
-        <Route path="/new" element={<Home111 />} />
-      </Routes>
-    </div>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   );
-};
+}
+
 export default App;
